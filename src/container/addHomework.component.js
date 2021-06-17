4a8650c7190da25d2f2c177f57173d4763cae20a
Mrunali Patil <mrunalipatil04@gmail.com>
2021-06-17 18:50:26 +0530

    Employee CRUS InProgress

--- src/container/addHomework.component.js
+++ src/container/addHomework.component.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Card} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 
 function AddHomeWork() {
-  
+
 
     return (
         <>
@@ -16,8 +16,18 @@ function AddHomeWork() {
                     </Card.Header>
                 </Card>
                 <Card.Body>
-                <h5>Code Pipeline Demo</h5>
-              </Card.Body>
+                    <h5>Code Pipeline Demo</h5>
+                    <Card>
+                        <Card.Header>
+                            <h5>Employee form</h5>
+                        </Card.Header>
+                        <Card.Body>
+                            <p>Employee Details: </p>
+                            <h5>Employee Name: John</h5>
+
+                        </Card.Body>
+                    </Card>
+                </Card.Body>
             </div>
 
 
