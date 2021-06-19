b64d58ca8f925d8be1f84e6720eab6b121880718
Mrunali Patil <mrunalipatil04@gmail.com>
2021-06-19 23:41:42 +0530

    master added homework file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-// import AddHomeWork from './container/addHomework.component';
-import Employee from './container/Employee.component';
+import AddHomeWork from './container/addHomework.component';
+// import Employee from './container/Employee.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from "react-router-dom";
 
@@ -8,9 +8,9 @@ function App() {
   return (
     <Switch>
 
-<Route exact path="/" component={Employee} />
+      <Route exact path="/" component={AddHomeWork} />
     </Switch>
-   
+
   );
 }
 
